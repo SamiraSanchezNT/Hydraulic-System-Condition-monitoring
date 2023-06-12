2e510ca56b53a51d753c418073703c452b8b7cfc
samirasancheze <samira.sanchez@newtwen.com>
2023-06-12 10:07:41 +0200

    Only stable measurements
    I excluded from measurement data all cases in which the temperature was not stable (profile[:,4] =1)

--- data/ploting.m
+++ data/ploting.m
@@ -1,33 +1,118 @@
 load('data_stable_subSample.mat')
 %%
-close all
-i =2
-tiledlayout('flow')
+% close all
+inter = [1, 481, 961, 1,     1
+        480, 960, 1449, 1449,1];
+j = 5;
+figure
+t1 = tiledlayout('flow');
+nexttile
+for ii = inter(1,j):10:inter(2,j)
+    plot(EPS1(ii,:))
+    hold on
+end
+title('EPS1')
+ylabel('W')
+xlabel('Time, s')
 
 nexttile
+for ii = inter(1,j):10:inter(2,j)
+plot(CE(ii,:))
+    hold on
+end
+title('CE')
+ylabel('%')
+xlabel('Time, s')
+% ylim([10 50])
+
+nexttile
+for ii = inter(1,j):10:inter(2,j)
+plot(CP(ii,:))
+    hold on
+end
+title('CP')
+ylabel('kW')
+xlabel('Time, s')
+% ylim([1 2.5])
+
+nexttile
+for ii = inter(1,j):10:inter(2,j)
+    plot(PS1(ii,:))
+    hold on
+end
+title('PS1')
+ylabel('bar')
+xlabel('Time, s')
+
+nexttile
+for ii = inter(1,j):10:inter(2,j)
+plot(PS2(ii,:))
+    hold on
+end
+title('PS2')
+ylabel('bar')
+xlabel('Time, s')
+
+nexttile
+for ii = inter(1,j):10:inter(2,j)
+plot(PS3(ii,:))
+    hold on
+end
+title('PS3')
+ylabel('bar')
+xlabel('Time, s')
+
+nexttile
+for ii = inter(1,j):10:inter(2,j)
+plot(TS1(ii,:))
+    hold on
+end
+title('TS1')
+% ylim([30 62])
+ylabel('^oC')
+xlabel('Time, s')
+
+nexttile
+for ii = inter(1,j):10:inter(2,j)
+plot(TS2(ii,:))
+    hold on
+end
+title('TS2')
+% ylim([30 62])
+ylabel('^oC')
+xlabel('Time, s')
+
+nexttile
+for ii = inter(1,j):10:inter(2,j)
+plot(FS1(ii,:))
+    hold on
+end
+title('FS1')
+ylabel('l/min')
+xlabel('Time, s')
+%%
+i = 1;
+figure
+subplot(3,2,1)
 plot(EPS1(i,:))
 title('EPS1')
 
-nexttile
+subplot(3,2,2)
 plot(PS1(i,:))
 title('PS1')
 
-nexttile
+subplot(3,2,4)
 plot(PS2(i,:))
 title('PS2')
 
-nexttile
+subplot(3,2,6)
 plot(PS3(i,:))
 title('PS3')
 
-nexttile
+subplot(3,2,3)
 plot(TS1(i,:))
 title('TS1')
 
-nexttile
+subplot(3,2,5)
 plot(TS2(i,:))
 title('TS2')
-
-nexttile
-plot(TS5(i,:))
-title('TS5')
